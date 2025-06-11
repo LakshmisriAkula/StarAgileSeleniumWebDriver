c7296d836870b1cd9567bc95e06edf7fa0424e43
LakshmisriAkula <akulalakshmisri@gmail.com>
2025-06-11 13:03:43 +0530

    token removed

--- SA2502045_restAssured/src/test/java/Authorization/BearerTokenAuthorization.java
+++ SA2502045_restAssured/src/test/java/Authorization/BearerTokenAuthorization.java
@@ -12,9 +12,9 @@ public class BearerTokenAuthorization {
 
 		String req_url = "https://api.github.com/user/repos";
 
-		String bearer_Token = "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";
+//		String bearer_Token = "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";
 
-		given().header("Authorization", "Bearer " + bearer_Token).when().get(req_url).then().log().body();
+//		given().header("Authorization", "Bearer " + bearer_Token).when().get(req_url).then().log().body();
 
 	}
 
